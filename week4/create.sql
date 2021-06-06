3827561ea3d0fceb904be67114e39d1983df90b2
Shernaz Nazaraliev <79134498+ShernazNazaraliev@users.noreply.github.com>
2021-06-06 09:13:35 +0300

    Update create.sql

--- week4/create.sql
+++ week4/create.sql
@@ -3,31 +3,31 @@ create database AutoShop;
 \c AutoShop;
 
 create table Cars(
-    ID integer primary key,
-    mark varchar(40) not null,
-    type varchar(40) not null,
-    color varchar(20) not null ,
-    engineVolume real not null ,
-    transferType varchar(20) not null ,
-    price double precision not null
+    CarID int unsigned primary key auto_increment,
+    CarName varchar(40) not null,
+    Color varchar(20) not null,
+    EngineVolume DOUBLE not null,
+    Prise DOUBLE not null
 );
+
 
 create table Customers(
-    ID serial primary key,
-    firstName varchar(40) not null ,
-    lastName varchar(40) not null ,
-    phoneNumber varchar(20) not null ,
-    email varchar(20) not null ,
-    address varchar(20) not null
+    CustomersId int unsigned primary key auto_increment,
+    CustomersName varchar(40) not null,
+    CustomersAddress varchar(40) not null,
+    CustomersEmail varchar(20) not null,
+    PhoneNamber varchar(15) not null,
+    unique(CustomersEmail, PhoneNamber)
 );
+
 
 create table Orders(
-    orderID serial primary key,
-    customersID integer,
-    carsID integer,
-    nameOrder varchar(20) not null,
-    createdDate date not null
+    OrdersID int unsigned primary key auto_increment,
+    CarID int unsigned,
+    CustomersId int unsigned,
+    DateOrder DATE,
+    foreign key (CustomersId) references Customers (CustomersId),
+    foreign key (CarID) references Cars(CarID)
 );
 
-ALTER TABLE Orders ADD CONSTRAINT fk_orders_customers FOREIGN KEY (customersID) REFERENCES Customers (ID);
-ALTER TABLE Orders ADD CONSTRAINT fk_orders_cars FOREIGN KEY (carsID) REFERENCES Cars (ID);
+
