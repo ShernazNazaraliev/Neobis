063edccdd3b5fd9c024dc0cad3ae8ca39a7b44ff
Shernaz Nazaraliev <79134498+ShernazNazaraliev@users.noreply.github.com>
2021-06-06 09:15:04 +0300

    Update Customers.java

--- week4/POJO/Customers.java
+++ week4/POJO/Customers.java
@@ -1,97 +1,58 @@
-package POJO;
-
-import java.util.Objects;
-
 public class Customers {
-    private long ID;
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String email;
-    private String address;
+    private long CustomersID;
+    private String CustomersName;
+    private String CustomersAddress;
+    private String CustomersEmail;
+    private String PhoneNamber;
 
-    public Customers(){
+    public Customers() {
     }
 
-    public Customers(long ID,String firstName, String lastName, String phoneNumber,String email, String address){
-        this.ID = ID;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
+    public Customers(long customersID, String name, String address, String email, String phoneNamber) {
+        CustomersID = customersID;
+        CustomersName = name;
+        CustomersAddress = address;
+        CustomersEmail = email;
+        PhoneNamber = phoneNamber;
     }
 
-    public void setID(long ID) {
-        this.ID = ID;
+    public void setCustomersID(long customersID) {
+        CustomersID = customersID;
     }
 
-    public long getID() {
-        return ID;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
+    public void setName(String name) {
+        CustomersName = name;
     }
 
     public void setAddress(String address) {
-        this.address = address;
+        CustomersAddress = address;
+    }
+
+    public void setEmail(String email) {
+        CustomersEmail = email;
+    }
+
+    public void setPhoneNamber(String phoneNamber) {
+        PhoneNamber = phoneNamber;
+    }
+
+    public long getCustomersID() {
+        return CustomersID;
+    }
+
+    public String getName() {
+        return CustomersName;
     }
 
     public String getAddress() {
-        return address;
+        return CustomersAddress;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customers customers = (Customers) o;
-        return ID == customers.ID && Objects.equals(firstName, customers.firstName) && Objects.equals(lastName, customers.lastName) && Objects.equals(phoneNumber, customers.phoneNumber) && Objects.equals(email, customers.email) && Objects.equals(address, customers.address);
+    public String getEmail() {
+        return CustomersEmail;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(ID, firstName, lastName, phoneNumber, email, address);
-    }
-
-    @Override
-    public String toString() {
-        return "Customers{" +
-                "ID=" + ID +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                '}';
+    public String getPhoneNamber() {
+        return PhoneNamber;
     }
 }
