071a9149599824d8dfb0021a64f7e07f420a3493
Shernaz Nazaraliev <79134498+ShernazNazaraliev@users.noreply.github.com>
2021-06-06 09:14:48 +0300

    Update Cars.java

--- week4/POJO/Cars.java
+++ week4/POJO/Cars.java
@@ -1,109 +1,59 @@
-package POJO;
-
-import java.math.BigDecimal;
-import java.util.Objects;
-
 public class Cars {
-    private long ID;
-    private String mark;
-    private String type;
-    private String color;
-    private Double engineVolume;
-    private String transferType;
-    private BigDecimal price;
+    private long CarID;
+    private String CarName;
+    private String Color;
+    private double EngineVolume;
+    private double Prise;
 
     public Cars() {
     }
 
-    public Cars(long ID, String mark, String type, String color, Double engineVolume, String transferType, BigDecimal price) {
-        this.ID = ID;
-        this.mark = mark;
-        this.type = type;
-        this.color = color;
-        this.engineVolume = engineVolume;
-        this.transferType = transferType;
-        this.price = price;
+    public Cars(long carID, String carName, String color, double engineVolume, double prise) {
+        CarID = carID;
+        CarName = carName;
+        Color = color;
+        EngineVolume = engineVolume;
+        Prise = prise;
     }
 
-    public long getID() {
-        return ID;
+    public void setCarID(long carID) {
+        CarID = carID;
     }
 
-    public String getMark() {
-        return mark;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Double getEngineVolume() {
-        return engineVolume;
-    }
-
-    public String getTransferType() {
-        return transferType;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setID(long ID) {
-        this.ID = ID;
-    }
-
-    public void setMark(String mark) {
-        this.mark = mark;
-    }
-
-    public void setType(String type) {
-        this.type = type;
+    public void setCarName(String carName) {
+        CarName = carName;
     }
 
     public void setColor(String color) {
-        this.color = color;
+        Color = color;
     }
 
-    public void setEngineVolume(Double engineVolume) {
-        this.engineVolume = engineVolume;
+    public void setEngineVolume(double engineVolume) {
+        EngineVolume = engineVolume;
     }
 
-    public void setTransferType(String transferType) {
-        this.transferType = transferType;
+    public void setPrise(double prise) {
+        Prise = prise;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
+    public long getCarID() {
+        return CarID;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cars cars = (Cars) o;
-        return ID == cars.ID && Objects.equals(mark, cars.mark) && Objects.equals(type, cars.type) && Objects.equals(color, cars.color) && Objects.equals(engineVolume, cars.engineVolume) && Objects.equals(transferType, cars.transferType) && Objects.equals(price, cars.price);
+    public String getCarName() {
+        return CarName;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(ID, mark, type, color, engineVolume, transferType, price);
+    public String getColor() {
+        return Color;
     }
 
-    @Override
-    public String toString() {
-        return "Cars{" +
-                "ID=" + ID +
-                ", mark='" + mark + '\'' +
-                ", type='" + type + '\'' +
-                ", color='" + color + '\'' +
-                ", engineVolume='" + engineVolume + '\'' +
-                ", transferType='" + transferType + '\'' +
-                ", price=" + price +
-                '}';
+    public double getEngineVolume() {
+        return EngineVolume;
+    }
+
+    public double getPrise() {
+        return Prise;
     }
 }
+
