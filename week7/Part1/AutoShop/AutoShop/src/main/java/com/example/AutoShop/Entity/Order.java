76d55c07148e3567285ed5cd8d31ae4548481e82
Shernaz Nazaraliev <shernazortokbaevich@gmail.com>
2021-08-08 16:50:53 +0600

    REST API

---
+++ week7/Part1/AutoShop/AutoShop/src/main/java/com/example/AutoShop/Entity/Order.java
@@ -0,0 +1,43 @@
+package com.example.AutoShop.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @SequenceGenerator(name = "orders_seq", sequenceName = "orders_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orders_seq")
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "customer_id")
+    private Customer customerId;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", referencedColumnName = "car_id")
+    private Car carId;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "date_of_creation")
+    private LocalDateTime actionDate;
+
+    public Order(Customer customerId, Car carId, String status) {
+        this.customerId = customerId;
+        this.carId = carId;
+        this.status = status;
+    }
+}
--- week7/Part1/AutoShop/AutoShop/src/main/java/com/example/AutoShop/Repository/CarRepository.java
+++ week7/Part1/AutoShop/AutoShop/src/main/java/com/example/AutoShop/Repository/CarRepository.java
@@ -3,5 +3,5 @@ package com.example.AutoShop.Repository;
 import com.example.AutoShop.Entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarRepository extends JpaRepository<Car,Long> {
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
